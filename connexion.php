f4676ceb6689627bca864bd68da7936bd9e326b6
daveiss <adaviditch@gmail.com>
2023-06-06 13:32:49 +0000

    second

---
+++ connexion.php
@@ -0,0 +1,19 @@
+<?php
+$host = 'localhost';
+$dbname = 'pdo';
+$username = 'root';
+$password = '';
+
+try {
+    $data = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+  
+    $data->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $data->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+ echo 'connexion';
+    
+} catch(PDOException $e) {
+    
+    echo "Erreur de connexion : " . $e->getMessage();
+}
+?>
