63fbf1bc8240031b9ffd74ec5b9d31ed79962d51
daveiss <adaviditch@gmail.com>
2023-06-12 14:06:22 +0000

    BDD

--- Maquette et base de données/application_dclic.sql
+++ Maquette et base de données/application_dclic.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jun 08, 2023 at 09:10 AM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Host: localhost:3306
+-- Generation Time: Jun 12, 2023 at 02:05 PM
+-- Server version: 8.0.30
+-- PHP Version: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,25 +24,42 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `adminis`
+--
+
+CREATE TABLE `adminis` (
+  `email` varchar(50) DEFAULT NULL,
+  `mot_de_passe` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `apprenant`
 --
 
 CREATE TABLE `apprenant` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL,
   `nom` varchar(50) DEFAULT NULL,
   `prenom` varchar(50) DEFAULT NULL,
-  `birthday` int(50) DEFAULT NULL,
+  `birthday` date DEFAULT NULL,
   `ville` varchar(50) DEFAULT NULL,
   `formation` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `apprenant`
 --
 
 INSERT INTO `apprenant` (`id`, `nom`, `prenom`, `birthday`, `ville`, `formation`) VALUES
-(1, 'fhcgv', 'hjvh', 1998, 'oslo', 'Astronomie'),
-(2, 'BAKO', 'Issognin', 1212, 'Canberra', 'Anthropologie');
+(1, 'BAMOUNI', 'Dieudonné', '1972-06-10', 'Réo', 'Management'),
+(2, 'BAMOUNI', 'Bertrant', '1998-06-10', 'Réo', 'Droit'),
+(3, 'NAMA', 'Donald', '1995-06-11', 'Sapouy', 'Art'),
+(4, 'BAMOUNI', 'Samson', '1965-06-11', 'Sapouy', 'Diplomatie'),
+(5, 'BAMOUNI', 'Ousmane', '1965-11-01', 'Leo', 'Secrétariat'),
+(6, 'BAMOUNI', 'Ousmane', '1965-11-01', 'Leo', 'Secrétariat'),
+(7, 'BAMOUNI', 'Dave', '1998-12-12', 'Oslo', 'Comptabilité'),
+(8, 'BAKO', 'Dave', '1998-12-12', 'Oslo', 'Comptabilité');
 
 --
 -- Indexes for dumped tables
@@ -62,7 +79,7 @@ ALTER TABLE `apprenant`
 -- AUTO_INCREMENT for table `apprenant`
 --
 ALTER TABLE `apprenant`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
